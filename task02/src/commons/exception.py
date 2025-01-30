a47ab29e1cb80134d3b5b01cc6230e2fbc869336
vtomi97 <verestomi97@gmail.com>
2025-01-30 13:16:44 +0100

    rm task02

--- task02/src/commons/exception.py
+++
@@ -1,8 +0,0 @@
-class ApplicationException(Exception):
-
-    def __init__(self, code, content):
-        self.code = code
-        self.content = content
-
-    def __str__(self):
-        return f'{self.code}:{self.content}'
