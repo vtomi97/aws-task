04deb464c7b76c7c8e0197cd50e8de620f19853c
vtomi97 <verestomi97@gmail.com>
2025-02-10 16:35:58 +0100

    task05

--- task05/src/lambdas/api_handler/handler.py
+++ task05/src/lambdas/api_handler/handler.py
@@ -1,16 +1,16 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
-#import boto3
-#import uuid
-#import json
-#from datetime import datetime
+import boto3
+import uuid
+import json
+from datetime import datetime
 
 
 _LOG = get_logger(__name__)
 
 
-#dynamodb = boto3.resource('dynamodb', region_name="eu-central-1")
-#table = dynamodb.Table("Events")
+dynamodb = boto3.resource('dynamodb', region_name="eu-central-1")
+table = dynamodb.Table("Events")
 
 
 class ApiHandler(AbstractLambda):
@@ -19,6 +19,19 @@ class ApiHandler(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
+        id = uuid.uuid4()
+        current_datetime = datetime.now()
+        iso_format = current_datetime.isoformat()
+
+        item = {
+            "id": id,
+            "principalId": 101,
+            "createdAt": iso_format,
+            "body": "test_body"
+        }
+        
+        table.put_item(Item=item)
+                
         return {"statusCode": 201}
         
     
