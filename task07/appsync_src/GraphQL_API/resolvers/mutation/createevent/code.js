5ced6bc0450dd10f41908c56cfc318345aae0c9a
vtomi97 <verestomi97@gmail.com>
2025-02-25 19:19:38 +0100

    Create code.js

---
+++ task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
@@ -0,0 +1,19 @@
+/**
+ * Sends a request to the attached data source
+ * @param {import('@aws-appsync/utils').Context} ctx the context
+ * @returns {*} the request
+ */
+export function request(ctx) {
+    // Update with custom logic or select a code sample.
+    return {};
+}
+
+/**
+ * Returns the resolver result
+ * @param {import('@aws-appsync/utils').Context} ctx the context
+ * @returns {*} the result
+ */
+export function response(ctx) {
+    // Update with response logic
+    return ctx.result;
+}
