c188ea25219a8d70b8207f0121aee0b6b917ca95
vtomi97 <verestomi97@gmail.com>
2025-02-25 21:09:44 +0100

    Update code.js

--- task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
+++ task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
@@ -1,18 +1,14 @@
-/**
- * Sends a request to the attached data source
- * @param {import('@aws-appsync/utils').Context} ctx the context
- * @returns {*} the request
- */
+import { utils } from '@aws-appsync/utils';
+import * as ddb from '@aws-appsync/utils/dynamodb'
+
+
 export function request(ctx) {
-    // Update with custom logic or select a code sample.
-    return {};
+    return ddb.put({
+		key: ctx.args.input,
+		item: "TEST"
+	});
 }
 
-/**
- * Returns the resolver result
- * @param {import('@aws-appsync/utils').Context} ctx the context
- * @returns {*} the result
- */
 export function response(ctx) {
     // Update with response logic
     return ctx.result;
