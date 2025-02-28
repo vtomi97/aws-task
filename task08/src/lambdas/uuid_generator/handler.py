7b5417d4b8fc4a45ba6895b331acb6fcc2e1cd82
vtomi97 <verestomi97@gmail.com>
2025-02-28 16:55:43 +0100

    task08

--- task08/src/lambdas/uuid_generator/handler.py
+++ task08/src/lambdas/uuid_generator/handler.py
@@ -1,5 +1,8 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
+import boto3
+import os
+from datetime import datetime
 
 _LOG = get_logger(__name__)
 
@@ -10,6 +13,16 @@ class UuidGenerator(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
+        current_datetime = datetime.now()
+        s3_client = boto3.client('s3')
+        bucket_name = os.environ.get('target_bucket')
+        file_name = current_datetime.isoformat()
+        file_content = ""
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key=file_name,
+            Body=file_content
+        )
         return 200
     
 
