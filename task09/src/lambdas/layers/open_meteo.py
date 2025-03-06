83e283da6f34c8eb043fe2b8249582ca50636345
vtomi97 <verestomi97@gmail.com>
2025-03-06 13:06:47 +0100

    task09

---
+++ task09/src/lambdas/layers/open_meteo.py
@@ -0,0 +1,14 @@
+#import requests
+
+class OpenMeteo:
+    #BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+
+    def get_weather(self):
+        #if params is None:
+         #   params = {}
+        #params['latitude'] = self.latitude
+        #params['longitude'] = self.longitude
+        # response = requests.get(self.BASE_URL)
+        #response.raise_for_status()
+        #return response.json()
+        return "OK"
